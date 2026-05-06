cd9ad9a27a6ddedba1468c4090f6ba35e6cfd297
Mahendra <132850243+mahifriends555@users.noreply.github.com>
2026-05-06 20:21:27 +0000

    update rag.py

--- src/rag.py
+++ src/rag.py
@@ -7,6 +7,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 pc = Pinecone(api_key=os.getenv("PINECONE_API_KEY"))
 index = pc.Index(os.getenv("PINECONE_INDEX_NAME"))
 openai_client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
